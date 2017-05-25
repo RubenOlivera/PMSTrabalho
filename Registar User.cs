82829d6b58779678204e2f20dd7eba886dc0f082
Ruben Oliveira <iscac15053@alumni.iscac.pt>
2017-05-25 22:47:43 +0100

    Registar

--- Registar User.cs
+++ Registar User.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SeniorNet_Events.Logica;
+using SeniorNet_Events.Modelos;
 
 namespace SeniorNet_Events
 {
@@ -28,8 +31,19 @@ namespace SeniorNet_Events
         }
 
         private void btnRegistar_Click(object sender, EventArgs e)
-        {
-
+        { 
+            
+            if (txtbPass.Text == txtbConfirmarPass.Text)
+            {
+                Utilizador u = Gestao_Utilizadores.Instance.Registarutil(txtbNome.Text, txtbPass.Text, txtbEmail.Text);
+                if (u != null)
+                {
+                    Principal_Log ev = new Principal_Log();
+                    ev.ShowDialog();
+                }
+                
+                
+            }
         }
     }
 }
